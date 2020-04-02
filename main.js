8919487a61dd3f9d452546b4938ebb9b8ff5202d
mateszenyan <56297654+mateszenyan@users.noreply.github.com>
2020-04-02 16:50:20 +0100

    commit

---
+++ main.js
@@ -0,0 +1,9 @@
+/* SCRIPTS.JS */
+
+function changePic() {
+    document.getElementById("profilePic").src = "images/photo.jpg";
+  }
+  
+  function normalPic() {
+    document.getElementById("profilePic").src = "images/normal.jpg";
+  }
